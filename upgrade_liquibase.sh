1388fd47f3319a0adfd6a8d4ed31f67c4b419932
Erik van der Goetz <erik.vandergoetz@verato.com>
2022-08-08 17:59:33 -0400

    upgrade liquibase 4.11.0

--- upgrade_liquibase.sh
+++ upgrade_liquibase.sh
@@ -4,7 +4,7 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 LBVERSION="4.11.0"
-JDBC_SNOWFLAKE="3.13.18"
+JDBC_SNOWFLAKE="3.13.21"
 JDBC_REDSHIFT="2.1.0.9"
 JDBC_BIGQUERY="1.2.25.1029"
 
@@ -23,9 +23,9 @@ wget "https://github.com/liquibase/liquibase-snowflake/releases/download/liquiba
 wget "https://repo1.maven.org/maven2/net/snowflake/snowflake-jdbc/${JDBC_SNOWFLAKE}/snowflake-jdbc-${JDBC_SNOWFLAKE}.jar"  -O "${DIR}/pyliquibase/jdbc-drivers/snowflake-jdbc-${JDBC_SNOWFLAKE}.jar"
 wget "https://repo1.maven.org/maven2/com/amazon/redshift/redshift-jdbc42/${JDBC_REDSHIFT}/redshift-jdbc42-${JDBC_REDSHIFT}.jar"  -O "${DIR}/pyliquibase/jdbc-drivers/redshift-jdbc42-${JDBC_REDSHIFT}.jar"
 
-wget "https://storage.googleapis.com/simba-bq-release/jdbc/SimbaJDBCDriverforGoogleBigQuery42_${JDBC_BIGQUERY}.zip" -O "${DIR}/SimbaJDBCDriverforGoogleBigQuery42_${JDBC_BIGQUERY}.zip"
-unzip -o "${DIR}/SimbaJDBCDriverforGoogleBigQuery42_${JDBC_BIGQUERY}.zip" -d "${DIR}/pyliquibase/jdbc-drivers"
-rm "SimbaJDBCDriverforGoogleBigQuery42_${JDBC_BIGQUERY}.zip"
+# wget "https://storage.googleapis.com/simba-bq-release/jdbc/SimbaJDBCDriverforGoogleBigQuery42_${JDBC_BIGQUERY}.zip" -O "${DIR}/SimbaJDBCDriverforGoogleBigQuery42_${JDBC_BIGQUERY}.zip"
+# unzip -o "${DIR}/SimbaJDBCDriverforGoogleBigQuery42_${JDBC_BIGQUERY}.zip" -d "${DIR}/pyliquibase/jdbc-drivers"
+# rm "SimbaJDBCDriverforGoogleBigQuery42_${JDBC_BIGQUERY}.zip"
 # commit to git
 git add --all
 git commit -m "upgrade liquibase ${LBVERSION}"
